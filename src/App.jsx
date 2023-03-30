6142d0a39a392d8d2594f7da0462abdc6f803b7b
Veljko <veljkoBitno@gmail.com>
2023-03-30 20:18:35 +0200

    Added images, dark mode and header styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,40 @@
+import { useState } from "react";
 import Switch from "react-switch";
-
 import "./styles/styles.scss";
 
 function App() {
+  const [checked, setChecked] = useState(false);
+
   return (
-    <>
+    <div className={checked ? "light" : "dark"}>
       <header className="header">
-        <h1 className="header__heading">Social Media Dashboard</h1>
-        <p className="header__text">Total Followers: 23,004</p>
+        <div className="header__content">
+          <h1 className="header__heading">Social Media Dashboard</h1>
+          <p className="header__text">Total Followers: 23,004</p>
+        </div>
 
         <div className="dark-mode">
           <p className="dark-mode__text">Dark Mode</p>
-          <Switch className="dark-mode__switch" />
+          <Switch
+            checked={checked}
+            uncheckedIcon={false}
+            checkedIcon={false}
+            width={48}
+            height={24}
+            handleDiameter={18}
+            onChange={() => setChecked(!checked)}
+            className="dark-mode__switch"
+          />
         </div>
       </header>
       <main>
         <div className="stats">
           <div className="stats__card" id="facebook">
-            <img className="stats__card__image" src="" alt="" />
+            <img
+              className="stats__card__image"
+              src="./src/images/icon-facebook.svg"
+              alt="Facebook"
+            />
             <p className="stats__card__handle">@nathanf</p>
             <p className="stats__card__text">
               <span>1987</span> Followers
@@ -26,8 +43,11 @@ function App() {
           </div>
 
           <div className="stats__card" id="twitter">
-            <img className="stats__card__image" src="" alt="" />
-
+            <img
+              className="stats__card__image"
+              src="./src/images/icon-twitter.svg"
+              alt="Twitter"
+            />
             <p className="stats__card__handle">@nathanf</p>
             <p className="stats__card__text">
               <span>1044</span> Followers
@@ -36,7 +56,11 @@ function App() {
           </div>
 
           <div className="stats__card" id="instagram">
-            <img className="stats__card__image" src="" alt="" />
+            <img
+              className="stats__card__image"
+              src="./src/images/icon-instagram.svg"
+              alt="Instagram"
+            />
             <p className="stats__card__handle">@realnathanf</p>
             <p className="stats__card__text">
               <span>11k</span> Followers
@@ -45,7 +69,11 @@ function App() {
           </div>
 
           <div className="stats__card" id="youtube">
-            <img className="stats__card__image" src="" alt="" />
+            <img
+              className="stats__card__image"
+              src="./src/images/icon-youtube.svg"
+              alt="Youtube"
+            />
             <p className="stats__card__handle">Nathan F.</p>
             <p className="stats__card__text">
               <span>8239</span> Subscribers
@@ -58,56 +86,88 @@ function App() {
           <div className="overview__cards">
             <div className="overview__card">
               <p className="overview__card__text">Page Views</p>
-              <img className="overview__card__logo" src="" alt="" />
+              <img
+                className="overview__card__logo"
+                src="./src/images/icon-facebook.svg"
+                alt="Facebook"
+              />
               <p className="overview__card__number">87</p>
               <p className="trend">3%</p>
             </div>
             <div className="overview__card">
               <p className="overview__card__text">Likes</p>
-              <img className="overview__card__logo" src="" alt="" />
+              <img
+                className="overview__card__logo"
+                src="./src/images/icon-facebook.svg"
+                alt="Facebook"
+              />
               <p className="overview__card__number">52</p>
               <p className="trend">2%</p>
             </div>
             <div className="overview__card">
               <p className="overview__card__text">Likes</p>
-              <img className="overview__card__logo" src="" alt="" />
+              <img
+                className="overview__card__logo"
+                src="./src/images/icon-instagram.svg"
+                alt="Instagram"
+              />
               <p className="overview__card__number">5462</p>
               <p className="trend">2257%</p>
             </div>
             <div className="overview__card">
               <p className="overview__card__text">Profile Views</p>
-              <img className="overview__card__logo" src="" alt="" />
+              <img
+                className="overview__card__logo"
+                src="./src/images/icon-instagram.svg"
+                alt="Instagram"
+              />
               <p className="overview__card__number">52k</p>
               <p className="trend">1375%</p>
             </div>
             <div className="overview__card">
               <p className="overview__card__text">Retweets</p>
-              <img className="overview__card__logo" src="" alt="" />
+              <img
+                className="overview__card__logo"
+                src="./src/images/icon-twitter.svg"
+                alt="Twitter"
+              />
               <p className="overview__card__number">117</p>
               <p className="trend">303%</p>
             </div>
             <div className="overview__card">
               <p className="overview__card__text">Likes</p>
-              <img className="overview__card__logo" src="" alt="" />
+              <img
+                className="overview__card__logo"
+                src="./src/images/icon-twitter.svg"
+                alt="Twitter"
+              />
               <p className="overview__card__number">507</p>
               <p className="trend">553%</p>
             </div>
             <div className="overview__card">
               <p className="overview__card__text">Likes</p>
-              <img className="overview__card__logo" src="" alt="" />
+              <img
+                className="overview__card__logo"
+                src="./src/images/icon-youtube.svg"
+                alt="Youtube"
+              />
               <p className="overview__card__number">107</p>
               <p className="trend">19%</p>
             </div>
             <div className="overview__card">
               <p className="overview__card__text">Total Views</p>
-              <img className="overview__card__logo" src="" alt="" />
+              <img
+                className="overview__card__logo"
+                src="./src/images/icon-youtube.svg"
+                alt="Youtube"
+              />
               <p className="overview__card__number">1407</p>
               <p className="trend">12%</p>
             </div>
           </div>
         </div>
       </main>
-    </>
+    </div>
   );
 }
 
